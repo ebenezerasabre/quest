699d3456a8ad47ffbfe9ea128b4e9e4a42c49daf
Ebenezer Asabre <ebenezerasabre@gmail.com>
2021-01-05 15:55:15 +0000

    Adding signInDriver route

--- routes/DriverRoutes.js
+++ routes/DriverRoutes.js
@@ -5,9 +5,10 @@ const DriverController = require('../controllers/DriverController');
 
 driverRouter.get('/', DriverController.findAllDrivers);
 driverRouter.get('/count', DriverController.countDrivers);
+driverRouter.get('/sign/:id', DriverController.signInDriver);
 driverRouter.get('/:id', DriverController.findDriverById);
-driverRouter.post('/', DriverController.createDriver);
 
+driverRouter.post('/', DriverController.createDriver);
 driverRouter.put('/:id', DriverController.updateDriver);
 driverRouter.delete('/:id', DriverController.deleteDriver);
 driverRouter.delete('/', DriverController.deleteAllDrivers);
